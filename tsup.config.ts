1b43bdcf9851c384600ae4f84369fcce1111e44c
Joe Hornby <joehornby@gmail.com>
2025-01-18 22:37:47 +0000

    chore: don't minify built js files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "tsup";
 
-const isDev = process.env.npm_lifecycle_event === "dev";
-
 export default defineConfig({
   clean: true,
-  minify: !isDev,
+  minify: false,
   bundle: true,
   entry: ["index.ts"],
   format: ["esm"],
